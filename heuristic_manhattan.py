46d6abbf1c554ed8544cb62d3b395277bd222c19
Marielouise Hanna <130917519+marielouisehanna@users.noreply.github.com>
2024-10-09 19:34:52 +0300

    Update heuristic_manhattan.py

--- heuristic_manhattan.py
+++ heuristic_manhattan.py
@@ -1,1 +1,12 @@
+class Manhattan:
+    def h(self, current_state, goal_state):
+        """Calculate Manhattan distance: sum of the vertical and horizontal distances of tiles from their goal positions"""
+        distance = 0
+        for i in range(3):
+            for j in range(3):
+                value = current_state[i][j]
+                if value != 0:  # Skip the blank tile
+                    goal_pos = [(goal_state == value).nonzero()][0]
+                    distance += abs(i - goal_pos[0][0]) + abs(j - goal_pos[1][0])
+        return distance
 
