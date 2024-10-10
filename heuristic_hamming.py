24bc295acb8d3f3d74f47bd3ee42119ed38389ef
Marielouise Hanna <130917519+marielouisehanna@users.noreply.github.com>
2024-10-10 14:42:31 +0300

    Update heuristic_hamming.py

--- heuristic_hamming.py
+++ heuristic_hamming.py
@@ -1,5 +1,14 @@
+# class Hamming:
+#     def h(self, current_state, goal_state):
+#         """Calculate Hamming distance: the number of misplaced tiles"""
+#         return sum(current_state[i][j] != goal_state[i][j] and current_state[i][j] != 0
+#                    for i in range(3) for j in range(3))
+
 class Hamming:
-    def h(self, current_state, goal_state):
-        """Calculate Hamming distance: the number of misplaced tiles"""
-        return sum(current_state[i][j] != goal_state[i][j] and current_state[i][j] != 0
-                   for i in range(3) for j in range(3))
+    """Hamming Distance heuristic: counts the number of misplaced tiles."""
+
+    @staticmethod
+    def h(current_state, goal_state):
+        """Calculates Hamming Distance between current state and goal state."""
+        hamming_count = sum(1 for i, j in zip(current_state.flatten(), goal_state.flatten()) if i != j and i != 0)
+        return hamming_count
